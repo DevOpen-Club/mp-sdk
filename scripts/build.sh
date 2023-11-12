88219378b086cd63087b2223855d9e25555b3199
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-11-13 00:36:53 +0800

    fix: 修复类型提示和源映射

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@ rm -rf es lib
 tsc --module ES2015 --outDir es
 tsc --module CommonJS --outDir lib
 
-copyfiles -u 1 "src/**/*.js" "src/**/*.d.ts" es
-copyfiles -u 1 "src/**/*.js" "src/**/*.d.ts" lib
+copyfiles -u 1 "src/**/*.js" "src/**/*.d.ts" "src/**/*.map" es
+copyfiles -u 1 "src/**/*.js" "src/**/*.d.ts" "src/**/*.map" lib
