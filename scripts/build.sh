37179931a034d44b8ab3f626df8b4523411d9fe6
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-11-15 22:25:57 +0800

    refactor: 静态资源放在 `assets` 目录中发布

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,3 @@ rm -rf es lib
 
 tsc --module ES2015 --outDir es
 tsc --module CommonJS --outDir lib
-
-copyfiles -u 1 "src/**/*.js" "src/**/*.d.ts" "src/**/*.map" es
-copyfiles -u 1 "src/**/*.js" "src/**/*.d.ts" "src/**/*.map" lib
